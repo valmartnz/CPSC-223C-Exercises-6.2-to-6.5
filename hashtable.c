48fdd39fbc49abeb6b896d68e38c1dcfd608d2d9
Valerie Martinez <47126912+valmartnz@users.noreply.github.com>
2020-04-20 13:10:44 -0700

    6.5 undef function added

--- hashtable.c
+++ hashtable.c
@@ -1,71 +1,102 @@
-//
-//  hashtable.c
-//  hashtable
-//
-//  Created by William McCarthy on 154//20.
-//  Copyright © 2020 William McCarthy. All rights reserved.
-//
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 
 #define HASHSIZE 101
 
-
-//-----------------------------------------------------------
 typedef struct hash_node {
   struct hash_node* next;
-  const char* name;
+  const char*name;
   char* defn;
 } hash_node;
 
 static hash_node* hashtab[HASHSIZE];
 
-//-----------------------------------------------------------
-unsigned hash(const char* s) {
-  unsigned hashval = 0;
-  
+unsigned hash(const char *s) {
+  unsigned hashval;
+
   for (hashval = 0; *s != '\0'; ++s) {
     hashval = *s + 31 * hashval;
   }
   return hashval % HASHSIZE;
 }
 
-//-----------------------------------------------------------
 hash_node* hash_lookup(const char* s) {
   hash_node* np;
-  
-  for (np = hashtab[hash(s)]; np != NULL; np = np->next) {
-    if (strcmp(s, np->name) == 0) { return np; }
-  }
-  
-  return NULL;
+
+  for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+    if (strcmp(s, np->name) == 0)
+      return np;  /* found */
+  return NULL;        /* not found */
 }
 
-//-----------------------------------------------------------
-hash_node* hash_install(const char* name, const char* defn) {
-  if (name == NULL) { return NULL; }
-  
-  hash_node* np;
-  unsigned hash_idx;
-  
-  if ((np = hash_lookup(name)) == NULL) { /* name not found, need new node */
-    np = (hash_node*)malloc(sizeof(hash_node));
-    if (np == NULL || (np->name = strdup(name)) == NULL) {
-      return NULL;
-    }
-    hash_idx = hash(name);
-    np->next = hashtab[hash_idx];
-    hashtab[hash_idx] = np;
-  } else {
-    free((void*)np->defn);
-  }
-  
-  if ((np->defn = strdup(defn)) == NULL) { return NULL; }
+char *strdup(const char *s) {   /* make a duplicate of s */
+  char *p;
 
+  p = (char*) malloc(strlen(s)+1);  /* +1 for ′\0′ */
+  if (p != NULL)
+    strcpy(p, s);
+  return p;
+}
+
+hash_node* hash_install(const char *name, const char *defn) {
+  if (name == NULL) { return NULL; }
+
+  hash_node* np;
+  unsigned hashval;
+
+  if ((np = hash_lookup(name)) == NULL) {  /* not found */
+    np = (hash_node*) malloc(sizeof(*np));
+    if (np == NULL || (np->name = strdup(name)) == NULL)
+      return NULL;
+    hashval = hash(name);
+    np->next = hashtab[hashval];
+    hashtab[hashval] = np;
+  } else      /* already there */
+    free((void*) np->defn);  /* free previous defn */
+  if ((np->defn = strdup(defn)) == NULL)
+    return NULL;
   return np;
+}
+
+// ============== 6.5 undef =============================================
+hash_node* hash_undef(const char *name) {
+  if (name == NULL) { return NULL; }
+
+  hash_node* found;
+
+  if ((found = hash_lookup(name)) == NULL) { return NULL; } /* not found and nothing to do */
+  else {
+    if (found->next != NULL) {
+      found->next = found->next->next;
+      found = found->next;
+    } else {
+      hashtab[hash(name)] = NULL;
+      free((void*) found);
+    }
+  }
+  return found;
+}
+// ======================================================================
+
+void print_defn(const char* s) {
+  hash_node* p = hash_lookup(s);
+  printf("'%s': ", s);
+  printf("%s\n", p ? p->defn : "not found");
+}
+
+void hash_print() {
+  for (int i = 0; i < HASHSIZE; ++i) {
+    printf("%3d...\n", i);
+    hash_node* np = hashtab[i];
+    while (np != NULL) {
+      printf("\t");
+      print_defn(np->name);
+      np = np->next;
+    }
+  }
 }
 
 void hash_deletenode(hash_node* p) {
@@ -92,50 +123,9 @@ void hash_clear() {
   printf("cleared\n");
 }
 
-void hash_undef(const char* name) {
-  if (name == NULL) { return; }
-  
-  hash_node* np;
-  unsigned hash_idx;
-  if ((np = hash_lookup(name)) == NULL) { return; }
-  
-  hash_idx = hash(name);
-  hash_node* p = np;
-  hash_node* prev = np;
-  while (p != np) {
-    prev = p;
-    p = p->next;
-  }
-  prev->next = p->next;
-  hash_deletenode(p);
-  if (hashtab[hash_idx]->next == NULL) {
-    hashtab[hash_idx] = NULL;
-  }
-}
-
-const char* yesorno(bool cond) { return cond ? "YES" : "no"; }
-
-void print_defn(const char* s) {
-  hash_node* p = hash_lookup(s);
-  printf("'%s': ", s);
-  printf("%s ", p ? p->defn : "not found");
-  printf(" ---> %p\n",  p ? p->next : NULL);
-}
-
-void hash_print(void) {
-  for (int i = 0; i < HASHSIZE; ++i) {
-    printf("%3d...\n", i);
-    hash_node* np = hashtab[i];
-    while (np != NULL) {
-      printf("\t");
-      print_defn(np->name);
-      np = np->next;
-    }
-  }
-}
-
 void test_hash_table() {
   printf("%s\n", "// ---------------------- TESTING HASH_TABLE ---------------------");
+
   hash_install("cat", "animal that likes fish");
   hash_install("dog", "animal that hates cats");
   hash_install("mouse", "animal eaten by cats");
@@ -152,20 +142,21 @@ void test_hash_table() {
   print_defn("virus");
 
   hash_print();
-  
+
   printf("\nredefining cat...\n");
   printf("redefining virus...\n");
-
   hash_install("cat", "animal that likes fish and mice and birds");
   hash_install("virus", "VERY EXPENSIVE pain in the neck");
-
   print_defn("cat");
   print_defn("virus");
   hash_print();
 
-  printf("undefining virus...\n");
-  hash_undef("virus");
-  print_defn("virus");
+  printf("\nredefining cat...\n");
+  printf("redefining dog...\n");
+  hash_undef("cat");
+  hash_install("dog", "animal that chases cats");
+  print_defn("cat");
+  print_defn("dog");
   hash_print();
 
   hash_clear();
@@ -194,7 +185,7 @@ void hash_test(int argc, const char* argv[]) {
   char delims[] = " \n";
   int size = 0;
   memset(buf, 0, sizeof(buf));
-  
+
   while (fgets(buf, BUFSIZ, fin)) {
     char* p = strtok(buf, delims);
     add_word_defn(p, "some defn", &size);
@@ -205,19 +196,16 @@ void hash_test(int argc, const char* argv[]) {
   }
   printf("%d words added...\n", size);
   hash_print();
-  
+
   hash_clear();
   hash_print();
-  
+
   fclose(fin);
 }
 
-//-----------------------------------------------------------
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
   test_hash_table();
-  
-//  hash_test(argc, argv);
-  
-  
+  hash_test(argc, argv);
+
   return 0;
 }
